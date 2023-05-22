33eefa7d8029613c625b2d4ffe5e4e9da6b2a0e6
oleg <58697249+zerex21@users.noreply.github.com>
2023-05-22 13:39:07 +0300

    feat: add check for input

--- task/script.js
+++ task/script.js
@@ -2,6 +2,9 @@ import {
     burgerToggle,
     closeBurger,
 } from "./scripts/burgerMenu.js";
+import { buttonSubmit } from "./scripts/checkInput.js";
+
 
 burgerToggle()
 closeBurger()
+buttonSubmit()
